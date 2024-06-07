cbdf9b006484efe3e61cf493d18b2ac3ffc14966
HieuNT-2306 <hieubeo2369@gmail.com>
2024-06-07 10:49:55 +0700

    Chỉnh lại file env

--- src/state/api.js
+++ src/state/api.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const api = createApi({ 
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL || 'http://localhost:5003/'}),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
     reducerPath: 'adminApi',
     tagTypes: ['User'],
     endpoints: (builder) => ({ 
